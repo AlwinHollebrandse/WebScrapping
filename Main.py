bd13c0245ac69706a9a876f6739af9fb7f9c45a8
AlwinHollebrandse <hollebrandsaj@mymail.vcu.edu>
2021-03-02 17:08:29 -0500

    made it scrap each page of monitors

--- Main.py
+++ Main.py
@@ -1,40 +1,64 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 import pandas as pd
 import time
 from pprint import pprint
 
-driver = webdriver.Firefox() # webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
-products=[] #List to store name of the product
-prices=[] #List to store price of the product
-ratings=[] #List to store rating of the product
-driver.get("https://www.flipkart.com/computers/monitors/pr?sid=6bo,9no&otracker=categorytree&fm=neo%2Fmerchandising&iid=M_0a4329f6-060a-460c-aff5-e710c5660a0e_1_372UD5BXDFYS_MC.ECL5SFI77NSY&otracker=hp_rich_navigation_3_1.navigationCard.RICH_NAVIGATION_Electronics~Computer%2BPeripherals~Monitors_ECL5SFI77NSY&cid=ECL5SFI77NSY")
+def main():
+    driver = webdriver.Firefox() # webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
+    products=[] #List to store name of the product
+    prices=[] #List to store price of the product
+    ratings=[] #List to store rating of the product
+    driver.get("https://www.flipkart.com/computers/monitors/pr?sid=6bo,9no&otracker=categorytree&fm=neo%2Fmerchandising&iid=M_0a4329f6-060a-460c-aff5-e710c5660a0e_1_372UD5BXDFYS_MC.ECL5SFI77NSY&otracker=hp_rich_navigation_3_1.navigationCard.RICH_NAVIGATION_Electronics~Computer%2BPeripherals~Monitors_ECL5SFI77NSY&cid=ECL5SFI77NSY")
 
-time.sleep(3) # per stackoverflow, there might be a race condition in the driver and this prevents it https://stackoverflow.com/questions/49734915/failed-to-decode-response-from-marionette-message-in-python-firefox-headless-s
-content = driver.page_source
-soup = BeautifulSoup(content)
-# print(soup.prettify()
-# print('all class: _1fQZEK')
-# # print(soup.findAll('a',href=True, attrs={'class':'_13oc-S'}))
-# pprint(soup.findAll(name='a',href=True, attrs={'class':'_1fQZEK'}))
+    # soup = BeautifulSoup(driver.page_source, 'html.parser')
 
-for a in soup.findAll(name='a',href=True, attrs={'class':'_1fQZEK'}): # _1AtVbE col-12-12
-    name=a.find('div', attrs={'class':'_4rR01T'})
-    price=a.find('div', attrs={'class':'_30jeq3 _1_WHN1'})
-    rating=a.find('div', attrs={'class':'_3LWZlK'})
-    products.append(name.text)
-    prices.append(price.text)
-    ratings.append(rating.text) 
+    # maxPageCount = int(html_soup.find('a', class_ = 'totalPageNum').text)+1
+    content = driver.page_source
 
-# TODO BUG sometimes the thing just desnt load...is it the sleep line?
-print('\nproducts')
-pprint(products) # matches the order of the web page
-temp = {'Product Name':products,'Price':prices,'Rating':ratings}
-print('\ntemp')
-print(temp)
-df = pd.DataFrame(temp) # TODO this matches the order of the html, but witout the "temp" it doesnt...
+    i = 0
 
-# df = pd.DataFrame({'Product Name':products,'Price':prices,'Rating':ratings}) # TODO why does this change the ordering of products? its not sorted on anything I can see
-print('\ndf')
-print(df)
-df.to_csv('products.csv', index=False, encoding='utf-8')
+    while True:
+        # TODO might be a selenium method
+        time.sleep(3) # per stackoverflow, there might be a race condition in the driver and this prevents it https://stackoverflow.com/questions/49734915/failed-to-decode-response-from-marionette-message-in-python-firefox-headless-s
+        content = driver.page_source
+        scrapPage(content, products, prices, ratings)
+        i += 1
+        print('page: ', i)
+
+        try:
+            driver.find_element_by_link_text('NEXT').click()
+        except:
+            print('Next link was not found')
+            break
+
+
+    # TODO BUG sometimes the thing just desnt load...is it the sleep line?
+    print('\nproducts')
+    pprint(products) # matches the order of the web page
+    temp = {'Product Name':products,'Price':prices,'Rating':ratings}
+    print('\ntemp')
+    print(temp)
+    df = pd.DataFrame(temp) # TODO this matches the order of the html, but witout the "temp" it doesnt...
+
+    # df = pd.DataFrame({'Product Name':products,'Price':prices,'Rating':ratings}) # TODO why does this change the ordering of products? its not sorted on anything I can see
+    print('\ndf')
+    print(df)
+    df.to_csv('products.csv', index=False, encoding='utf-8')
+
+def scrapPage(content, products, prices, ratings):
+    soup = BeautifulSoup(content)
+    for a in soup.findAll(name='a',href=True, attrs={'class':'_1fQZEK'}):
+        name=a.find('div', attrs={'class':'_4rR01T'})
+        price=a.find('div', attrs={'class':'_30jeq3 _1_WHN1'})
+        rating=a.find('div', attrs={'class':'_3LWZlK'})
+        products.append(name.text)
+        prices.append(price.text)
+        if rating == None:
+            ratings.append('No Ratings')
+        else:
+            ratings.append(rating.text) 
+
+if __name__ == "__main__":
+    main()
